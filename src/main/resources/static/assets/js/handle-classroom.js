0ffc1b3ebcb662d0f7190f15e138a72262867c02
shreeramk1998 <shreeramk.1998@gmail.com>
2020-03-21 03:28:28 +0530

    added websocket connection

--- src/main/resources/static/assets/js/handle-classroom.js
+++ src/main/resources/static/assets/js/handle-classroom.js
@@ -1,3 +1,13 @@
+var ws ;
 $("#classSel").change(function(){
 	$('iframe').attr('src',"http://"+$(this).val());
+	ws.send("browser");
+});
+
+$(function(){
+	ws = new WebSocket('ws://localhost:8080/socket');
+	ws.onmessage = function(data) {
+		console.log(data);
+	}
+	
 });
